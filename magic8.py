8ddcceb149a93a8757362cf9d855c0f0cb9d49c5
AmberJohnsonPurdueGlobal <107158761+AmberJohnsonPurdueGlobal@users.noreply.github.com>
2023-01-02 01:57:20 -0800

    Update magic8.py

--- magic8.py
+++ magic8.py
@@ -1,9 +1,10 @@
+#allows integers to be chosen at random
 import random
 
 #Generate username
 name = ""
 #Generate Question to ask
-question = "Will today be a good day?"
+question = ""
 #Generate series of possible answers
 answer = ""
 #Random question picker
@@ -11,33 +12,45 @@ random_number = random.randint(1, 9)
 #uncomment to verify random answer picker is picking at random
 #print(random_number)
 
-#if user does not input name it replaces the name with "seeker"
-if name == "":
+#if user does not input name or question
+#"Seeker, ask and I shall tell"
+if name == "" and question == "":
+  print("Seeker, ask and I shall tell")
+  #If there is no name but a question 
+  #"Seeker asks: question"
+elif name == "":
   print("Seeker"+ " asks: " + question)
+  #name is providedand question is not
+  #"name, ask me a question!"
+elif name == name and question == "":
+  print(name + " ask me a question!")
+  #both name and question provided
+  #"Name asks: question"
 else:
    print(name + " asks: " + question)
-#Ask a question
 
-print("magic8 states: ")
+#After question is asked the magic8 redponds uless there is no question asked
+if question != "":
+  print("magic8 states: ")
 
-#series of poosible answers within the 1-9 range to output
-if random_number == 1:
+#series of poosible answers within the 1-9 range to output only if a question is ASKED
+if random_number == 1 and question != "":
   print("Yes - definitely.")
-elif random_number == 2:
+elif random_number == 2 and question != "":
   print("It is decidedly so.")
-elif random_number == 3:
+elif random_number == 3 and question != "":
   print("Without a doubt.")
-elif random_number == 4:
+elif random_number == 4 and question != "":
   print("Reply hazy, try again.")
-elif random_number == 5:
-  print("sk again later.")
-elif random_number == 6:
+elif random_number == 5 and question != "":
+  print("Ask again later.")
+elif random_number == 6 and question != "":
   print("Better not tell you now.")
-elif random_number == 7:
+elif random_number == 7 and question != "":
   print("My sources say no.")
-elif random_number == 8:
+elif random_number == 8 and question != "":
   print("Outlook not so good.")
-elif random_number == 9:
+elif random_number == 9 and question != "":
   print("Very doubtful.")
   #answer if outside range of 1-9 answers to ooutput
 else:
